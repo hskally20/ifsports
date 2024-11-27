f18cc67fad7fe528e4be6e8ab5e7b154617e70c6
sk <herculesskally81@gmail.com>
2024-11-27 10:04:35 -0300

    ajustandando responsividade e layaudy

---
+++ static/js/meu.js
@@ -0,0 +1,16 @@
+// Obtém o botão de menu e a sidebar
+const menuToggle = document.querySelector('.menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+// Adiciona um evento de clique no botão para alternar a visibilidade da sidebar
+menuToggle.addEventListener('click', () => {
+    sidebar.classList.toggle('active');  // Ativa ou desativa a sidebar
+    menuToggle.classList.toggle('active');  // Ativa ou desativa o botão de menu
+});
+
+// Fecha a sidebar ao clicar no botão de fechar
+const closeBtn = document.querySelector('.sidebar .close-btn');
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('active');  // Remove a classe active para fechar a sidebar
+    menuToggle.classList.remove('active');  // Altera o estado do botão de menu
+});
