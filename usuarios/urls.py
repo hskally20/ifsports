bffec2decfb80e55f3e6c80b493d25d61e48f13b
sk <herculesskally81@gmail.com>
2024-12-05 14:39:57 -0300

    resgiostra  wip

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LogoutView
-
+from .views import UsuarioCreate
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('profile/', views.profile_view, name='profile'),
-   path('registrar/', views.UsuarioCreate.as_view(), name='registrar'),
+    path('registrar/', UsuarioCreate.as_view(), name='registrar'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -8,32 +8,20 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.forms import UserCreationForm
-
-
-class UsuarioForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
-
-    def clean_confirm_password(self):
-        password = self.cleaned_data.get('password')
-        confirm_password = self.cleaned_data.get('confirm_password')
-
-        if password != confirm_password:
-            raise forms.ValidationError("As senhas não coincidem.")
-        return confirm_password
-
-
+class UsuarioCreate(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'registrar.html'
+    success_url = reverse_lazy('login')
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)  # Faz o login automaticamente após o registro
+        return redirect(self.success_url)
 
 # Função de Logout
 def logout_view(request):
     logout(request)
     return redirect('login')  # Redireciona para a página de login após o logout
-
-
 
 def login_view(request):
     if request.method == "POST":
@@ -46,22 +34,3 @@ def login_view(request):
         else:
             messages.error(request, "Usuário ou senha inválidos. Tente novamente.")
     return render(request, 'login.html')
-
-# Página de Perfil (necessário estar logado para acessar)
-@login_required
-def profile_view(request):
-    return render(request, 'usuarios/profile.html')
-
-
-# Registro de novo usuário (usando CreateView)
-
-class UsuarioCreate(CreateView):
-    model = User
-    form_class = UserCreationForm
-    template_name = 'registrar.html'
-    success_url = reverse_lazy('login')
-    def form_valid(self, form):
-        user = form.save()
-        auth_login(self.request, user)
-        return redirect(self.success_url)
-
